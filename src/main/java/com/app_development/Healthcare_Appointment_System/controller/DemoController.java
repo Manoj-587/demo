8998342cfbf95caf3fe487e0dedae0e0d9ceac2f
Manoj-587 <mkumar07959@gmail.com>
2025-08-29 11:56:45 +0530

    first commit

--- src/main/java/com/app_development/Healthcare_Appointment_System/controller/DemoController.java
+++ src/main/java/com/app_development/Healthcare_Appointment_System/controller/DemoController.java
@@ -1,6 +1,6 @@
 package com.app_development.Healthcare_Appointment_System.controller;
 
-public class demo {
+public class DemoController {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
